3da3488a41f1280ea34e2a983446e7ce41d1031c
PauloCalor <114022696+PauloCalor@users.noreply.github.com>
2024-02-22 10:57:42 -0300

    Add files via upload

---
+++ ClassesHerois.py
@@ -0,0 +1,31 @@
+class Heroi:
+    def __init__(self, nome, idade, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+    
+    def atacar(self):
+        ataque = ""
+        if self.tipo == "mago":
+            ataque = "usou magia"
+        elif self.tipo == "guerreiro":
+            ataque = "usou espada"
+        elif self.tipo == "monge":
+            ataque = "usou artes marciais"
+        elif self.tipo == "ninja":
+            ataque = "usou shuriken"
+        
+        print(f"O {self.tipo} atacou usando {ataque}")
+
+# Exemplo de uso da classe Heroi
+heroi1 = Heroi("Gandalf", 1020, "mago")
+heroi1.atacar()  # Saída: O mago atacou usando magia
+
+heroi2 = Heroi("Aragorn", 35, "guerreiro")
+heroi2.atacar()  # Saída: O guerreiro atacou usando espada
+
+heroi3 = Heroi("Bruce Lee", 32, "monge")
+heroi3.atacar()  # Saída: O monge atacou usando artes marciais
+
+heroi4 = Heroi("Hattori Hanzo", 45, "ninja")
+heroi4.atacar()  # Saída: O ninja atacou usando shuriken
